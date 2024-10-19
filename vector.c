4d353ff00e7b1e43b9cf8981a3473672f26a13fb
Naördisch <bogdankhamidullin@gmail.com>
2024-10-19 13:30:30 +0500

    Vector with append implementation

---
+++ vector.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Vector_ {
+	unsigned int size;
+	unsigned int amount;
+	int *array;
+} Vector;
+
+Vector CreateVector(unsigned int size, int fill, int values) {
+	Vector new_vector;
+	new_vector.size = size;
+	new_vector.array = malloc(size * sizeof(new_vector.array));
+	
+	for (unsigned int i = 0; i < size; ++i) {
+		new_vector.array[i] = values;
+	}
+	
+	switch (fill){
+		case 1:
+			new_vector.amount = size;
+			break;
+		case 0:
+			new_vector.amount = 0;
+			break;
+	}
+	
+	return new_vector;
+}
+
+void AppendToVector(Vector *the_vector, int item) {
+	if (the_vector->amount + 1 > the_vector->size) {
+		unsigned int new_size = the_vector->size * 2;
+		int *new_array;
+		new_array = malloc(new_size * sizeof(new_array));
+		for (unsigned int i = 0; i < the_vector->size; ++i){
+			new_array[i] = the_vector->array[i];
+		}
+		free(the_vector->array);
+		the_vector->array = new_array;
+		the_vector->size = new_size;
+	}
+	the_vector->array[the_vector->amount] = item;
+	++the_vector->amount;
+}
+
+void DeleteVector(Vector *vector_to_be_deleted){
+	free(vector_to_be_deleted->array);
+}
+
+int main() {
+	Vector vector_a = CreateVector(5, 1, 0);
+	for (unsigned int i = 0; i < vector_a.amount; ++i) {
+		printf("%d ", vector_a.array[i]);
+	}
+	printf("\n");
+	AppendToVector(&vector_a, 10);
+		for (unsigned int i = 0; i < vector_a.amount; ++i) {
+		printf("%d ", vector_a.array[i]);
+	}
+	printf("\n");
+	DeleteVector(&vector_a);
+}
